efae3c508de777277f3cedfbe755755867caaeee
andreivedovato1 <andrei.vedovato@opah.com.br>
2023-02-06 14:04:52 -0300

    add about page and link it

---
+++ app/about/head.tsx
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <title>About | Nextjs 13 Ga Partytown Experiment</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta name="description" content="About page demo" />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  );
+}
--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,10 +1,9 @@
 import Link from "next/link";
-
 export default function Home() {
   return (
     <main>
-      <h1>Hello, World</h1>
-      <Link href="/about">Sobre</Link>
+      <h1>About page</h1>
+      <Link href="/">Voltar</Link>
     </main>
   );
 }
