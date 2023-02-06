3350d1a1342d1a3fa4aa195a56749ec7d5479999
andreivedovato1 <andrei.vedovato@opah.com.br>
2023-02-06 14:17:49 -0300

    update strategy

--- components/Analytics.tsx
+++ components/Analytics.tsx
@@ -7,14 +7,14 @@ export default function Analytics() {
         async
         id="ga"
         src="https://www.googletagmanager.com/gtag/js?id=G-57JLVPDLMS"
-        strategy="worker"
+        strategy="lazyOnload"
         type="text/partytown"
       />
 
       <Script
         id="ga_dataLayer"
         type="text/partytown"
-        strategy="worker"
+        strategy="lazyOnload"
         dangerouslySetInnerHTML={{
           __html:
             "window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-57JLVPDLMS');",
