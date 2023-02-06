0fe4438bab8a676aef5655a7594932d10804ef42
andreivedovato1 <andrei.vedovato@opah.com.br>
2023-02-06 14:53:52 -0300

    make GA call as script

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,32 +2,42 @@
 
 import { useEffect } from "react";
 import Link from "next/link";
+import Script from "next/script";
 
 export default function Home() {
-  useEffect(() => {
-    if (typeof window !== "undefined") {
-      console.info("heehee");
-      // @ts-ignore
-      window.dataLayer.push({
-        event: "page_view_xablau",
-        page_title: "HEE HEE TESTE",
-        page_location: window.location.href,
-        page_path: window.location.pathname,
-        send_to: "G-57JLVPDLMS",
-      });
+  // useEffect(() => {
+  //   if (typeof window !== "undefined") {
+  //     console.info("heehee");
+  //     // @ts-ignore
+  //     window.dataLayer.push({
+  //       event: "page_view_xablau",
+  //       page_title: "HEE HEE TESTE",
+  //       page_location: window.location.href,
+  //       page_path: window.location.pathname,
+  //       send_to: "G-57JLVPDLMS",
+  //     });
 
-      // @ts-ignore
-      window.dataLayer.push("event", "screen_view", {
-        app_name: "myAppName",
-        screen_name: "Home",
-      });
-    }
-  }, []);
+  //     // @ts-ignore
+  //     window.dataLayer.push("event", "screen_view", {
+  //       app_name: "myAppName",
+  //       screen_name: "Home",
+  //     });
+  //   }
+  // }, []);
 
   return (
     <main>
       <h1>About page</h1>
       <Link href="/">Voltar</Link>
+
+      <Script
+        id="ga_dataLayer2"
+        type="text/partytown"
+        dangerouslySetInnerHTML={{
+          __html:
+            "gtag('event', 'screen_view', { 'app_name': 'myAppName', 'screen_name': 'Home' });",
+        }}
+      />
     </main>
   );
 }
